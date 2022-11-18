bcd3757de55374f88939c6e611b1afb01c3991bb
Nursultan Kozhogulov <kozhogulovn00@gmail.com>
2022-11-18 18:15:06 +0300

    Add some code

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	apiserver "github.com/Nursultank00/avito_test_task"
 	"github.com/Nursultank00/avito_test_task/pkg/handler"
 	"github.com/Nursultank00/avito_test_task/pkg/repository"
 	"github.com/Nursultank00/avito_test_task/pkg/service"
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
@@ -18,17 +20,13 @@ func main() {
 		log.Fatalf("error occured while initializing config: %s", err.Error())
 	}
 
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("error occured while loading env variables: %s", err.Error())
-	}
-
 	db, err := repository.NewPostgresDB(&repository.Config{
 		Host:     viper.GetString("db.host"),
 		Port:     viper.GetString("db.port"),
 		Username: viper.GetString("db.username"),
 		DBName:   viper.GetString("db.dbname"),
 		SSLMode:  viper.GetString("db.sslmode"),
-		Password: os.Getenv("DB_PASSWORD"),
+		Password: viper.GetString("db.password"),
 	})
 
 	if err != nil {
@@ -38,8 +36,23 @@ func main() {
 	service := service.NewService(repos)
 	handler := handler.NewHandler(service)
 	srv := apiserver.NewServer(viper.GetString("port"), handler.InitRoutes())
-	if err := srv.Run(); err != nil {
-		log.Fatalf("error occured while running http server: %s", err.Error())
+
+	go func() {
+		if err := srv.Run(); err != nil {
+			log.Fatalf("error occured while running http server: %s", err.Error())
+		}
+	}()
+
+	log.Print("Server started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	log.Print("Server shutting down")
+
+	if err := srv.ShutDown(context.Background()); err != nil {
+		log.Fatalf("error occured on server shutting down %s", err.Error())
 	}
 }
 
