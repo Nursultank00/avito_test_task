9bcb00d3fbba0108581efb86521cad4ac214ffdf
Nursultan Kozhogulov <kozhogulovn00@gmail.com>
2022-11-18 14:49:10 +0300

    Add trans_type field

--- models/account.go
+++ models/account.go
@@ -1,7 +1,7 @@
 package models
 
 type Account struct {
-	AccountId      int `json:"account_id"`
-	MainBalance    int `json:"main_balance"`
-	ReserveBalance int `json:"reserve_balance"`
+	AccountId      int `json:"account_id" db:"account_id"`
+	MainBalance    int `json:"main_balance" db:"main_balance"`
+	ReserveBalance int `json:"reserve_balance" db:"reserve_balance"`
 }
--- models/transactions.go
+++ models/transactions.go
@@ -3,10 +3,12 @@ package models
 import "time"
 
 type Transactions struct {
-	TransactionId int       `json:"transasction_id"`
-	Account_id    int       `json:"account_id"`
-	ServiceId     int       `json:"service_id"`
-	OrderId       int       `json:"order_id"`
-	Amount        int       `json:"amount"`
-	Datetime      time.Time `json:"datetime"`
+	TransactionId int       `json:"transasction_id" db:"transasction_id"`
+	AccountId     int       `json:"account_id" db:"account_id"`
+	ServiceId     int       `json:"service_id" db:"service_id"`
+	OrderId       int       `json:"order_id" db:"order_id"`
+	Amount        int       `json:"amount" db:"amount"`
+	Datetime      time.Time `json:"datetime" db:"datetime"`
+	TransType     string    `json:"trans_type" db:"trans_type"`
+	Description   string    `json:"description" db:"description"`
 }
